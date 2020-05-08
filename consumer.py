46008648bdedaa71d19bef86d1398f759dafaf3a
Danilo Vargas <danilovargas.01@gmail.com>
2020-05-08 17:35:45 -0500

    add consumer of queue

---
+++ consumer.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+import aio_pika
+import ujson
+
+from settings import RABBIT_USER, RABBIT_PASSWORD, RABBIT_HOST
+
+logger = logging.getLogger('hawk')
+
+
+class Consumer:
+
+    @classmethod
+    async def consume(cls, queue_name, callback):
+        connection = await aio_pika.connect_robust(
+            f'amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}/', loop=asyncio.get_event_loop()
+        )
+
+        async with connection:
+            channel = await connection.channel()
+            queue = await channel.declare_queue(
+                queue_name,
+                auto_delete=False,
+                durable=True
+            )
+            async with queue.iterator() as queue_iter:
+                async for message in queue_iter:
+                    async with message.process(requeue=True):
+                        body = ujson.loads(message.body)
+                        logger.debug('consuming messages from queue', extra={'queue': queue_name, 'body': body})
+                        callback(body)
+                        logger.debug('callback done from queue', extra={'queue': queue_name})
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+
+RABBIT_USER = os.environ['RABBIT_USER']
+RABBIT_PASSWORD = os.environ['RABBIT_PASSWORD']
+RABBIT_HOST = os.environ['RABBIT_HOST']
