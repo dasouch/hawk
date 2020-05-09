491a208651f5c1f1354f0a0a8f55f768328754b8
Danilo Vargas <danilovargas.01@gmail.com>
2020-05-08 21:19:46 -0500

    implement setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+install_requires = [
+    'aio-pika==6.6.0',
+    'ujson==2.0.3'
+]
+
+
+test_require = [
+    'pytest==5.4.2',
+    'pytest-asyncio==0.12.0',
+    'pytest-cov==2.8.1',
+    'pytest-mock==3.1.0'
+]
+
+setup(
+    name='hawk',
+    version='0.0.1',
+    packages=['hawk'],
+    install_requires=install_requires,
+    author='Danilo Vargas',
+)
