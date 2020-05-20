0eddeec5bd4505e6c3aad5e7c30791db4087c8e8
Danilo Vargas <danilovargas.01@gmail.com>
2020-05-20 13:31:30 -0500

    Implement context manager in class of Publisher

--- hawk/publisher.py
+++ hawk/publisher.py
@@ -12,20 +12,28 @@ logger = logging.getLogger('hawk')
 
 class Publisher:
 
-    @classmethod
-    async def send_message(cls, queue_name, data: Dict):
+    def __init__(self):
+        self._connection = None
+
+    async def __aenter__(self):
+        self._connection = await aio_pika.connect_robust(f"amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}/",
+                                                         loop=asyncio.get_event_loop())
+        return self
+
+    async def send_message(self, queue_name, data: Dict):
         logger.debug(f'creating send queue {queue_name}', extra=data)
-        connection = await aio_pika.connect_robust(f"amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}/",
-                                                   loop=asyncio.get_event_loop())
-        async with connection:
-            channel = await connection.channel()
-            await channel.declare_queue(
-                queue_name,
-                auto_delete=False,
-                durable=True
-            )
-            await channel.default_exchange.publish(
-                aio_pika.Message(body=ujson.dumps(data).encode()),
-                routing_key=queue_name
-            )
-            logger.debug(f'success send queue {queue_name}', extra=data)
+
+        channel = await self._connection.channel()
+        await channel.declare_queue(
+            queue_name,
+            auto_delete=False,
+            durable=True
+        )
+        await channel.default_exchange.publish(
+            aio_pika.Message(body=ujson.dumps(data).encode()),
+            routing_key=queue_name
+        )
+        logger.debug(f'success send queue {queue_name}', extra=data)
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self._connection.close()
