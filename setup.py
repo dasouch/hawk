f0a47bf3a6b00915edb2ad4cc7904026c3702d05
Danilo Vargas <danilovargas.01@gmail.com>
2022-07-26 20:28:21 -0500

    update version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 install_requires = [
-    'aio-pika==5.6.0',
+    'aio-pika==8.1.0',
     'ujson==3.0.0'
 ]
 
@@ -16,7 +16,7 @@ test_require = [
 
 setup(
     name='hawk',
-    version='1.1.2',
+    version='1.1.3',
     packages=['hawk'],
     install_requires=install_requires,
     author='Danilo Vargas',
