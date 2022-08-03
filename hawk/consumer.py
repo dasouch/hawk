a085ad388a9b6429ba2d555dd6de21887072fc16
Danilo Vargas <danilovargas.01@gmail.com>
2022-08-03 08:52:55 -0500

    Fixed bug in consumer

--- hawk/consumer.py
+++ hawk/consumer.py
@@ -11,7 +11,6 @@ class Consumer:
 
     def __init__(self):
         self._callbacks = {}
-        self._connection = None
         self.loop = asyncio.get_event_loop()
 
     def add_consumer_callback(self, queue_name, callback):
@@ -23,16 +22,18 @@ class Consumer:
             f"amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}/{RABBIT_VIRTUAL_HOST}")
 
     async def consume(self):
-        self._connection = await self.get_connection()
-        async with self._connection:
-            tasks = []
-            for queue_name, callback in self._callbacks.items():
-                tasks.append(self._consumer(queue_name=queue_name, callback=callback))
-            await asyncio.gather(*tasks)
+        tasks = []
+        for queue_name, callback in self._callbacks.items():
+            tasks.append(self._consumer(queue_name=queue_name, callback=callback))
+        await asyncio.gather(*tasks)
 
     async def _consumer(self, queue_name, callback):
-        channel: Channel = await self._connection.channel()
+        connection = await self.get_connection()
+        channel = await connection.channel()
         await channel.set_qos(prefetch_count=1)
         queue = await channel.declare_queue(queue_name, durable=True, auto_delete=False)
         await queue.consume(callback=callback)
-        await asyncio.Future()
+        try:
+            await asyncio.Future()
+        finally:
+            await connection.close()
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_require = [
 
 setup(
     name='hawk',
-    version='1.1.7',
+    version='1.1.8',
     packages=['hawk'],
     install_requires=install_requires,
     author='Danilo Vargas',
