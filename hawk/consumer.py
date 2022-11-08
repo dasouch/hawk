41f36ab3b7fce067c824a2730ada5ca82daf15a3
Danilo Vargas <danilovargas.01@gmail.com>
2022-11-08 10:05:08 -0500

    Fixed bug in gather

--- hawk/consumer.py
+++ hawk/consumer.py
@@ -30,10 +30,7 @@ class Consumer:
             tasks = []
             for queue_name, callback in self._callbacks.items():
                 tasks.append(asyncio.create_task(self._consumer(queue_name=queue_name, callback=callback)))
-            try:
-                await asyncio.gather(*tasks)
-            except asyncio.CancelledError:
-                pass
+            await asyncio.gather(*tasks, return_exceptions=True)
 
     async def _consumer(self, queue_name, callback):
         queue = await self._channel.declare_queue(queue_name, durable=True, auto_delete=False)
