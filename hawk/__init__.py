8ec8bcfb4bb05b30f88d74df05041a606492070b
Danilo Vargas <danilovargas.01@gmail.com>
2022-11-11 13:03:42 -0500

    Implement asynchronous message processing

--- hawk/__init__.py
+++ hawk/__init__.py
@@ -1,2 +1,3 @@
 from hawk.consumer import Consumer  # noqa: F401
+from hawk.decorator import parse_message  # noqa: F401
 from hawk.publisher import Publisher  # noqa: F401
--- hawk/consumer.py
+++ hawk/consumer.py
@@ -1,7 +1,6 @@
 import asyncio
 
 import aio_pika
-import ujson
 from aio_pika.abc import AbstractRobustConnection
 
 from hawk.settings import RABBIT_USER, RABBIT_PASSWORD, RABBIT_HOST, RABBIT_VIRTUAL_HOST
@@ -12,6 +11,7 @@ class Consumer:
     def __init__(self):
         self._callbacks = {}
         self.loop = asyncio.get_event_loop()
+        self._connection = None
         self._channel = None
 
     def add_consumer_callback(self, queue_name, callback):
@@ -23,10 +23,10 @@ class Consumer:
             f"amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}/{RABBIT_VIRTUAL_HOST}")
 
     async def consume(self):
-        _connection = await self.get_connection()
-        async with _connection:
-            self._channel = await _connection.channel()
-            await self._channel.set_qos(prefetch_count=10)
+        self._connection = await self.get_connection()
+        async with self._connection:
+            self._channel = await self._connection.channel()
+            await self._channel.set_qos(prefetch_count=100)
             tasks = []
             for queue_name, callback in self._callbacks.items():
                 tasks.append(asyncio.create_task(self._consumer(queue_name=queue_name, callback=callback)))
@@ -34,7 +34,9 @@ class Consumer:
 
     async def _consumer(self, queue_name, callback):
         queue = await self._channel.declare_queue(queue_name, durable=True, auto_delete=False)
-        async with queue.iterator() as queue_iter:
-            async for message in queue_iter:
-                async with message.process():
-                    await callback(ujson.loads(message.body))
+        await queue.consume(callback=callback)
+
+        try:
+            await asyncio.Future()
+        finally:
+            await self._connection.close()
