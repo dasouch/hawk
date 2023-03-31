14b990d4a68eeff19267a46faa4100df24a7ba68
Danilo Vargas <danilovargas.01@gmail.com>
2023-03-30 19:13:16 -0500

    Release/1.4.0 (#14)
    * Feature/upgrade async consumer (#12)
    
    * upgrade consumer
    
    * Fixed tests
    
    * Feature/upgrade dependencies (#13)
    
    * upgrade requirements
    
    * upgrade setup

--- tests/integration_tests/test_consumer.py
+++ tests/integration_tests/test_consumer.py
@@ -1,18 +1,24 @@
 from pytest import mark
 
-from hawk import Publisher
+from hawk import Publisher, QueueCallback
 from hawk.consumer import Consumer
 
 
 @mark.asyncio
 async def test_consumer_success(event_loop):
-    async with Publisher(service='queue') as publisher:
+    async with Publisher() as publisher:
         await publisher.send_message('test_queue', data={'first_name': 'Danilo', 'last_name': 'Vargas'})
+        await publisher.send_message('one_queue', data={'first_name': 'Danilo', 'last_name': 'Vargas'})
 
-    async def queue_callback(data):
-        print('data: ', data)
+    class TestQueueCallback(QueueCallback):
+        async def handle(self, message: dict):
+            assert message
 
-    consumer = Consumer(service='queue')
-    consumer.add_consumer_callback(queue_name='test_queue', callback=queue_callback)
-    consumer.add_consumer_callback(queue_name='one_queue', callback=queue_callback)
+    class TestOneQueueCallback(QueueCallback):
+        async def handle(self, message: dict):
+            assert message
+
+    consumer = Consumer()
+    consumer.add_consumer_callback(queue_name='test_queue', callback=TestQueueCallback())
+    consumer.add_consumer_callback(queue_name='one_queue', callback=TestOneQueueCallback())
     await consumer.consume()
