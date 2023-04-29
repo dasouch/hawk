9e57b3675b8616e5ccd047c77f71ac8d18e86e86
Danilo Vargas <danilovargas.01@gmail.com>
2023-04-28 19:23:27 -0500

    Hotfix/add fallback in queue callback (#20)
    * add fallback
    
    * upgrade setup

--- hawk/callback.py
+++ hawk/callback.py
@@ -11,5 +11,12 @@ class QueueCallback:
         raise NotImplementedError
 
     async def handle_message(self, message: aio_pika.IncomingMessage):
-        async with message.process():
-            await self.handle(message=ujson.loads(message.body))
+        try:
+            async with message.process():
+                await self.handle(message=ujson.loads(message.body))
+        except Exception as exc:
+            await self.fallback(exc)
+
+    @abc.abstractmethod
+    async def fallback(self, exc):
+        pass
