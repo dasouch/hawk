197ce07d05ff717e57e878ed29127022e23a2b73
Danilo Vargas <danilovargas.01@gmail.com>
2023-04-28 23:55:51 -0500

    Refactor of connection

--- hawk/callback.py
+++ hawk/callback.py
@@ -3,8 +3,25 @@ import abc
 import aio_pika
 import ujson
 
+from hawk.settings import RABBIT_USER, RABBIT_PASSWORD, RABBIT_HOST, RABBIT_VIRTUAL_HOST
+
 
 class QueueCallback:
+    def __init__(self, queue_name, prefetch_count: int = 100):
+        self.queue_name = queue_name
+        self.prefetch_count = prefetch_count
+
+    async def __aenter__(self):
+        self.connection = await aio_pika.connect(
+            f"amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}/{RABBIT_VIRTUAL_HOST}")
+        self.channel = await self.connection.channel()
+        await self.channel.set_qos(prefetch_count=self.prefetch_count)
+        self.queue = await self.channel.declare_queue(self.queue_name, auto_delete=False, durable=True)
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.channel.close()
+        await self.connection.close()
 
     @abc.abstractmethod
     async def handle(self, message: dict):
--- hawk/consumer.py
+++ hawk/consumer.py
@@ -1,41 +1,23 @@
 import asyncio
 
-import aio_pika
-from aio_pika.abc import AbstractRobustConnection
-
 from hawk.callback import QueueCallback
-from hawk.settings import RABBIT_USER, RABBIT_PASSWORD, RABBIT_HOST, RABBIT_VIRTUAL_HOST
 
 
 class Consumer:
 
-    def __init__(self, prefetch_count: int = 100):
-        self._callbacks = {}
+    def __init__(self):
+        self._callbacks = []
         self.loop = asyncio.get_event_loop()
-        self._connection = None
-        self._channel = None
-        self._prefetch_count = prefetch_count
 
-    def add_consumer_callback(self, queue_name, callback: QueueCallback):
-        self._callbacks[queue_name] = callback
+    def add_consumer_callback(self, callback: QueueCallback):
+        self._callbacks.append(callback)
 
     @staticmethod
-    async def get_connection() -> AbstractRobustConnection:
-        return await aio_pika.connect_robust(
-            f"amqp://{RABBIT_USER}:{RABBIT_PASSWORD}@{RABBIT_HOST}/{RABBIT_VIRTUAL_HOST}")
+    async def consume_from_queue(queue_handler: QueueCallback):
+        async with queue_handler:
+            await queue_handler.queue.consume(queue_handler.handle_message)
+            await asyncio.Future()
 
-    @staticmethod
-    async def consume_from_queue(queue_name, queue_handler: QueueCallback, channel):
-        queue = await channel.declare_queue(queue_name, auto_delete=False, durable=True)
-        await queue.consume(queue_handler.handle_message)
-
-    async def consume(self):
-        self._connection = await self.get_connection()
-        async with self._connection:
-            channel = await self._connection.channel()
-            await channel.set_qos(prefetch_count=self._prefetch_count)
-
-            tasks = [asyncio.create_task(self.consume_from_queue(queue_name, queue_handler, channel)) for
-                     queue_name, queue_handler in self._callbacks.items()]
-
-            await asyncio.wait(tasks)
+    async def consume(self) -> None:
+        tasks = [asyncio.create_task(self.consume_from_queue(queue_handler)) for queue_handler in self._callbacks]
+        await asyncio.gather(*tasks)
