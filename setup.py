ac132d7404ebc61f548dabe414e36ca2d343a9a7
Danilo Vargas <danilovargas.01@gmail.com>
2023-04-28 19:22:53 -0500

    upgrade setup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_require = [
 
 setup(
     name='hawk',
-    version='1.4.5',
+    version='1.4.6',
     packages=['hawk'],
     install_requires=install_requires,
     author='Danilo Vargas',
