b994627b33648ae3e749497011c5b3d67ce751b8
Danilo Vargas <danilovargas.01@gmail.com>
2023-06-05 19:36:48 -0500

    upgrade tests

--- tests/integration_tests/test_consumer.py
+++ tests/integration_tests/test_consumer.py
@@ -1,24 +1,32 @@
 from pytest import mark
 
 from hawk import Publisher, QueueCallback
+from hawk.connection import Connection
 from hawk.consumer import Consumer
 
 
 @mark.asyncio
 async def test_consumer_success(event_loop):
-    async with Publisher() as publisher:
+    connection = Connection(user='guest', password='guest', host='127.0.0.1')
+    async with Publisher(connection=connection) as publisher:
         await publisher.send_message('test_queue', data={'first_name': 'Danilo', 'last_name': 'Vargas'})
         await publisher.send_message('one_queue', data={'first_name': 'Danilo', 'last_name': 'Vargas'})
 
     class TestQueueCallback(QueueCallback):
+        async def fallback(self, exc):
+            pass
+
         async def handle(self, message: dict):
             assert message
 
     class TestOneQueueCallback(QueueCallback):
+        async def fallback(self, exc):
+            pass
+
         async def handle(self, message: dict):
             assert message
 
-    consumer = Consumer()
-    consumer.add_consumer_callback(queue_name='test_queue', callback=TestQueueCallback())
-    consumer.add_consumer_callback(queue_name='one_queue', callback=TestOneQueueCallback())
+    consumer = Consumer(connection=connection)
+    consumer.add_consumer_callback(callback=TestQueueCallback(queue_name='test_queue'))
+    consumer.add_consumer_callback(callback=TestOneQueueCallback(queue_name='one_queue'))
     await consumer.consume()
--- tests/integration_tests/test_publisher.py
+++ tests/integration_tests/test_publisher.py
@@ -1,9 +1,11 @@
 from pytest import mark
 
+from hawk.connection import Connection
 from hawk.publisher import Publisher
 
 
 @mark.asyncio
 async def test_publisher_success(event_loop):
-    async with Publisher() as publisher:
+    connection = Connection(user='guest', password='guest', host='127.0.0.1')
+    async with Publisher(connection=connection) as publisher:
         await publisher.send_message('test_queue', data={'first_name': 'Danilo', 'last_name': 'Vargas'})
