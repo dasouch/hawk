d97f0ba2e47b0b969e9cbe2dfb3861010c6fc4ab
Danilo Vargas <danilovargas.01@gmail.com>
2023-09-04 22:26:29 -0500

    upgrade connection

--- hawk/connection.py
+++ hawk/connection.py
@@ -4,20 +4,22 @@ import aio_pika
 
 
 class Connection:
-    def __init__(self, user: AnyStr, password: AnyStr, host: AnyStr, virtual_host: Optional[AnyStr] = None):
+    def __init__(self, user: AnyStr, password: AnyStr, host: AnyStr, virtual_host: Optional[AnyStr] = None,
+                 reconnect_interval: int = 5):
         self.user = user
         self.password = password
         self.host = host
         self.virtual_host = virtual_host
         self.connection = None
+        self.reconnect_interval = reconnect_interval
 
     async def connect(self):
+        connection = f"amqp://{self.user}:{self.password}@{self.host}"
+
         if self.virtual_host:
-            self.connection = await aio_pika.connect_robust(
-                f"amqp://{self.user}:{self.password}@{self.host}/{self.virtual_host}")
-        else:
-            self.connection = await aio_pika.connect_robust(
-                f"amqp://{self.user}:{self.password}@{self.host}")
+            connection += f"/{self.virtual_host}"
+
+        self.connection = await aio_pika.connect_robust(connection, reconnect_interval=self.reconnect_interval)
 
     async def channel(self):
         return await self.connection.channel()
